5078914f603b1af1232ee21920c045fc4ea257ea
davidboren <david.boren@avant.com>
2016-03-05 12:59:28 -0800

    TryCatch random lockbox test

--- tests/testthat/test-random-dependencies.R
+++ tests/testthat/test-random-dependencies.R
@@ -3,8 +3,10 @@ context('random-dependencies')
 test_that('Our random selection of CRAN and syberia packages can install', {
   test_dir <- tempfile()
   dir.create(test_dir)
-  is_success <- test_random_lockbox(test_dir, num_random_cran = 1, seed = 0)$success
-  print(is_success[[2]])
+  is_success <- tryCatch(
+    test_random_lockbox(test_dir, num_random_cran = 1, seed = 0)$success
+    , error = function(e) e)
+  print(is_success)
   unlink(test_dir, TRUE, TRUE)
   expect_true(is_success)
 })

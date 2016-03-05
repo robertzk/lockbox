ce90bf553d02a9a545c514919c851efe9ca43c2d
davidboren <david.boren@avant.com>
2016-03-05 12:51:34 -0800

    Find out why travis is failing

--- tests/testthat/test-random-dependencies.R
+++ tests/testthat/test-random-dependencies.R
@@ -3,7 +3,8 @@ context('random-dependencies')
 test_that('Our random selection of CRAN and syberia packages can install', {
   test_dir <- tempfile()
   dir.create(test_dir)
-  is_success <- test_random_lockbox(test_dir, num_random_cran = 10, seed = 0)$success
+  is_success <- test_random_lockbox(test_dir, num_random_cran = 1, seed = 0)$success
+  print(is_success[[2]])
   unlink(test_dir, TRUE, TRUE)
   expect_true(is_success)
 })

f8b02ed6d401a505020f505012699aaab17c7b6d
davidboren <david.boren@avant.com>
2016-03-05 17:38:02 -0800

    Finalize test formatting

--- tests/testthat/test-random-dependencies.R
+++ tests/testthat/test-random-dependencies.R
@@ -3,10 +3,7 @@ context('random-dependencies')
 test_that('Our random selection of CRAN and syberia packages can install', {
   test_dir <- tempfile()
   dir.create(test_dir)
-  is_success <- tryCatch(
-    test_random_lockbox(test_dir, num_random_cran = 1, seed = 0)$success
-    , error = function(e) e)
-  print(is_success)
+  test_output <- test_random_lockbox(test_dir, num_random_cran = 1, seed = 0)
   unlink(test_dir, TRUE, TRUE)
-  expect_true(is_success)
+  expect_true(test_output$success)
 })
